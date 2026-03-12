f76f23cd1765293293cb410fc5bd7b3f3f4b83de
Sekar243 <kinasekin6602@gmail.com>
2026-03-12 23:56:43 +0700

    Menambahkan fitur membuka koneksi database

--- PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.btnConnect.TabIndex = 0;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
             // btnHitungMhs
             // 
--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -32,7 +32,19 @@ namespace PraktikumADO
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                MessageBox.Show("Koneksi ke database berhasil");
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
