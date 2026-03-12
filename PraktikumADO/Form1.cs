d39f4a91eeba00fd8dce3d60cdff5d931157882a
Sekar243 <kinasekin6602@gmail.com>
2026-03-13 00:05:19 +0700

    Menambahkan fitur menghitung jumlah mata kuliah

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -69,7 +69,21 @@ namespace PraktikumADO
 
         private void btnHitungMk_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
