f3702dace9355f9a236acd2a014474ad8f1a01bc
Sekar243 <kinasekin6602@gmail.com>
2026-03-13 00:21:25 +0700

    Menambahkan fitur menghitung jumlah dosen

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -107,7 +107,22 @@ namespace PraktikumADO
 
         private void btnHitungDosen_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                string query = "SELECT COUNT(*) FROM Dosen";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         }
     }
 }
